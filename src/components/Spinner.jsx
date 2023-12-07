beb96c70ada1728b50caa870cfafa9eab91df40b
JayIrv <135241031+JIrv30@users.noreply.github.com>
2023-12-07 21:34:36 +0000

    initial commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+export const Spinner = () => {
+  return (
+    <>
+      <div className="spinner">
+        
+      </div>
+      <h1 className=" text-2xl text-center text-blue-800 pt-4"> Thank you for Submitting your resource</h1>
+    </>
+  )
+}
