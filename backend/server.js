e2edabc8989da5a8fca699a06e55fb0467ce8e47
JayIrv <135241031+JIrv30@users.noreply.github.com>
2023-12-10 12:31:15 +0000

    reciving first response from db and displaying

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const cors = require('cors')
 
 //express app
 const app = express()
@@ -10,10 +11,13 @@ const app = express()
 //middleware to log requests to console
 app.use(express.json())
 
+app.use(cors())
+
 app.use((req, res, next)=>{
   console.log(req.path, req.method)
   next()
 })
+
 
 //routes
 app.use('/api/workouts',workoutRoutes)
